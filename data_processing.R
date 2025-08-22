269a44e7e67ae14379cce65a44c75cf9ced1015e
Ava <aklissouras@unc.edu>
2025-08-22 07:18:25 -0400

    Feature selection using filtering / information criteria

--- data_processing.R
+++ data_processing.R
@@ -3,9 +3,10 @@
 library(mlr3)
 library(mlr3filters)
 library(FSelectorRcpp)
+library(dplyr)
 
 #Apply feature selection to JUST training set to prevent data leakage
-full_train = read.csv("credit_train.csv")
+train_full = read.csv("credit_train.csv")
 
 cat_vars = c("checkings", "credit_history", "purpose", "savings", 
              "employment_duration", "installment_rate", 
@@ -13,12 +14,12 @@ cat_vars = c("checkings", "credit_history", "purpose", "savings",
              "property", "other_installment_plans","housing",
              "number_credits","job","people_liable","telephone",
              "foreign_worker","credit_risk")
-full_train = full_train %>% mutate (across(all_of(cat_vars), as.factor))
+train_full = train_full %>% mutate (across(all_of(cat_vars), as.factor))
 
 #Turn this classification problem into a task object
 task = TaskClassif$new(
   id="pred_risk", 
-  backend = full_train, 
+  backend = train_full, 
   target = "credit_risk")
 
 #Use information gain to filter the data
@@ -33,7 +34,7 @@ feature_score_df = data.frame(
 feature_score_df = feature_score_df %>% arrange(desc(score))
 topfeatures = head(feature_score_df, n = 16)$feature
 #Create new training dataset - training, feature selection, information gain
-train_fs_ig = train %>% select(any_of(topfeatures), credit_risk)
+train_fs_ig = train_full %>% select(any_of(topfeatures), credit_risk)
 
 #Now, let's see if multicollinearity is reduced from before
 full_model = glm(credit_risk ~., data = train_fs_ig, 
@@ -42,3 +43,16 @@ library(car)
 vifs = vif(full_model)
 vifs
 #Much better, now they're all less than 4 :) 
+
+#Also select the same features from validation/testing sets
+#read in CSV's
+valid_full = read.csv("credit_valid.csv")
+test_full = read.csv("credit_test.csv")
+#Select features from each dataset
+valid_fs_ig = valid_full %>% select(any_of(topfeatures), credit_risk)
+test_fs_ig = test_full %>% select(any_of(topfeatures), credit_risk)
+#Write train_fs_ig, valid_fs_ig, test_fs_ig, to new CSV files to be read in for creating classifier models
+write.csv(train_fs_ig, "train_fs_ig.csv", row.names=FALSE)
+write.csv(valid_fs_ig, "valid_fs_ig.csv", row.names=FALSE)
+write.csv(test_fs_ig, "test_fs_ig.csv", row.names=FALSE)
+
