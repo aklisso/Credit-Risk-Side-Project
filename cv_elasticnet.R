b471ba97063fcb307d30b1e853a9c7425581dbe1
Ava <aklissouras@unc.edu>
2025-08-26 07:20:08 -0400

    Rerun using feature-selected dataset

--- cv_elasticnet.R
+++ cv_elasticnet.R
@@ -1,28 +1,23 @@
 #Read in CSV's created in cleaning_eda.R
-train <- read.csv("credit_train.csv")
-valid <- read.csv("credit_valid.csv")
-test <- read.csv("credit_test.csv")
+train <- read.csv("train_fs_ig.csv")
+valid <- read.csv("valid_fs_ig.csv")
+test <- read.csv("test_fs_ig.csv")
 
-#Standardize numeric variables: duration, amount, age
-
-#Train
+#Standardize numeric variables: duration, amount (note: age removed in feature seln earlier)
 library(dplyr)
 train.s = train %>% mutate (duration.s = (duration-mean(duration))/sd(duration))
 train.s = train.s %>% mutate (amount.s = (amount-mean(amount))/sd(amount))
-train.s = train.s %>% mutate (age.s = (age-mean(age))/sd(age))
-train.s = train.s %>% select(-c(duration, amount, age))
+train.s = train.s %>% select(-c(duration, amount))
 
 #Validation
 valid.s = valid %>% mutate (duration.s = (duration-mean(duration))/sd(duration))
 valid.s = valid.s %>% mutate (amount.s = (amount-mean(amount))/sd(amount))
-valid.s = valid.s %>% mutate (age.s = (age-mean(age))/sd(age))
-valid.s = valid.s %>% select(-c(duration, amount, age))
+valid.s = valid.s %>% select(-c(duration, amount))
 
 #Test
 test.s = test %>% mutate (duration.s = (duration-mean(duration))/sd(duration))
 test.s = test.s %>% mutate (amount.s = (amount-mean(amount))/sd(amount))
-test.s = test.s %>% mutate (age.s = (age-mean(age))/sd(age))
-test.s = test.s %>% select(-c(duration, amount, age))
+test.s = test.s %>% select(-c(duration, amount))
 
 #elasticnet regression with cross validation on training data
 library(glmnet)
@@ -32,7 +27,7 @@ y = train.s$credit_risk
 
 #Find best lambda using CV
 credit_elasticnet = cv.glmnet(x=X, y=y, alpha=1, family=binomial(link="logit"))
-cv_elasticnet_lambda = credit_elasticnet$lambda.1se #best MSE achieved when lambda = 0.01738841
+cv_elasticnet_lambda = credit_elasticnet$lambda.1se #best MSE achieved when lambda = 0.02936188
 
 #Coefficients for elasticnet
 coef(credit_elasticnet, s = cv_elasticnet_lambda)
@@ -50,23 +45,17 @@ yv = valid.s$credit_risk
 #Must convert to vector because predict() returns a matrix
 en_v_pred = as.vector(predict(credit_elasticnet, Xv, s = cv_elasticnet_lambda, type= "response"))
 cv_elastic_roc = roc(valid$credit_risk, en_v_pred)
-auc(cv_elastic_roc) #AUC is 0.7486 - not awful, not amazing
-#Try to find cutoff for event that maximizes sensitivity/specificity
+auc(cv_elastic_roc) #AUC is 0.7589
+#Try to find cutoff for event that maximizes sensitivity/specificity: 0.6443343
 opt_cutoff = coords(cv_elastic_roc, "best", ret = "threshold")
-#More stringent threshold than 0.5 for events (~0.6150302)
 
 #Get predicted classes (0,1) for validation data
-en_v_class = ifelse(en_v_pred > 0.6150302, 1, 0)
+en_v_class = ifelse(en_v_pred > opt_cutoff[[1]], 1, 0)
 
 #Confusion matrix
 library(caret)
-confusionMatrix(table(en_v_class,valid$credit_risk))
-#Everything looks OK to me except PPV (probability someone pred. risky truly is risky) is a bit low
-#This means the bank could miss out on $$$ from loan interest if they don't approve the loan
+confusionMatrix(table(en_v_class,valid$credit_risk), positive="1")
 
-#Next: potentially use this as a base model and stack models to improve AUC
-#NOTE: I might have to come back to this with data that has been processed using feature selection (FS)
-# to better reduce multicollinearity
 
 
 
